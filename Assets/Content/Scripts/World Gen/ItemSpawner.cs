c50b0538d30379caece3542574ab877166e6222e
ConorsFlying <53551115+ConorsFlying@users.noreply.github.com>
2024-01-27 17:52:22 +1100

    Before new Model

--- Assets/Content/Scripts/World Gen/ItemSpawner.cs
+++ Assets/Content/Scripts/World Gen/ItemSpawner.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 
 namespace Jam
 {
     public class ItemSpawner : MonoBehaviour
     {
+        [SerializeField] private ChunkSpawner m_chunkSpawner;
+        
         [SerializeField] private GameObject[] m_objects;
         [SerializeField] private float m_spawnRate = 0.2f;
+        [SerializeField] private bool m_SpawnPhysicsItems = false;
         
         [Range(0f, 1f)]
         [SerializeField] private float m_spawnRandomizer = 0.6f;
@@ -27,24 +34,44 @@ namespace Jam
         {
             m_deltaTime += Time.deltaTime;
 
-            if(m_deltaTime > m_spawnRate)
+            if (m_deltaTime > m_spawnRate)
             {
                 m_deltaTime = 0;
 
-                if(Random.value > m_spawnRandomizer)
+                if (Random.value > m_spawnRandomizer)
                 {
-                    int index = Random.Range(0, m_objects.Length);
-
-                    GameObject item = Instantiate(m_objects[index]);
-                    item.SetActive(true);
-
-                    item.transform.position = transform.position + new Vector3(0, 0, GetRandomSpecificNumber());
-                    item.transform.rotation = Quaternion.Euler(rotation);
-
-                    MovingItem objectScipt = item.GetComponent<MovingItem>();
-                    objectScipt.moveDirection = moveDirection;
-                    objectScipt.movingSpeed = Random.Range(movingSpeed.x, movingSpeed.y);
+                    SpawnObjectsInSequence();
                 }
+            }
+        }
+        
+        private async void SpawnObjectsInSequence()
+        {
+            int zPosition = GetRandomSpecificNumber(); // Cache the Z position for all spawns
+
+            for (int i = 0; i < 3; i++) // Spawn 3 objects
+            {
+                int index = Random.Range(0, m_objects.Length);
+                GameObject item = Instantiate(m_objects[index]);
+                item.SetActive(true);
+
+                // Use the cached Z position
+                item.transform.position = transform.position + new Vector3(0, 0, zPosition); 
+                item.transform.rotation = Quaternion.Euler(rotation);
+
+                if (m_SpawnPhysicsItems)
+                {
+                    var movingItem = item.AddComponent<MovingPhysicsItem>();
+                    movingItem.moveDirection = moveDirection;
+                    movingItem.movingSpeed = Random.Range(movingSpeed.x, movingSpeed.y);
+                }
+                else
+                {
+                    var movingItem = item.AddComponent<MovingItem>();
+                    movingItem.spawner = m_chunkSpawner;
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(0.1f));
             }
         }
         
---
+++ Assets/Content/Scripts/World Gen/MovingPhysicsItem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+
+namespace Jam
+{
+    public class MovingPhysicsItem : MonoBehaviour
+    {                
+        public Vector3 moveDirection = new Vector3(1, 0, 0);    //Set by spawner after instantiating
+        public float movingSpeed = 1;   //Set by spawner after instantiating
+        public float ExtraYGravity = -100f;
+        
+        Rigidbody rigidBody;
+
+
+        void Start()
+        {
+            rigidBody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            rigidBody.MovePosition(transform.position + moveDirection * movingSpeed * Time.deltaTime * movingSpeed);
+            rigidBody.AddForce(Vector3.up * ExtraYGravity, ForceMode.Force);
+
+            if (transform.position.y < -300)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            if (collision.rigidbody)
+            {
+                Vector3 force = (Vector3.up * 2 + Random.insideUnitSphere).normalized * Random.Range(100, 150);
+                collision.rigidbody.AddForce(force, ForceMode.Impulse);
+            }
+        }
+    }
+}
