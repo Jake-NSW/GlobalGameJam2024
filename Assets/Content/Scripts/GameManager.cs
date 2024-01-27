3b44a7a5a3ecc3e1ed90230eefa83d124bde8035
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2024-01-27 17:32:05 +1100

    stuff v2

--- Assets/Content/Scripts/GameManager.cs
+++ Assets/Content/Scripts/GameManager.cs
@@ -1,14 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 namespace Jam
 {
     public sealed class GameManager : MonoBehaviour
     {
-        public void LevelFromIndex(int level)
+        public static GameManager Instance { get; private set; }
+
+        public ToiletGoal Goal;
+
+        public void Awake()
         {
-            
+            Instance = this;
+        }
+
+        public int Points;
+
+        public float SpeedMultiplier = 1;
+
+        public float TimeInLevel;
+
+        private void Start()
+        {
+            TimeInLevel = 0;
+            Goal.transform.position = new Vector3(0, 0, 100);
+        }
+
+        private void Update()
+        {
+            TimeInLevel += Time.deltaTime * SpeedMultiplier;
+
+            // Start with scaling down goal, then once around the 10 second mark use position
+            var scale = Vector3.Lerp(Vector3.one * 50, Vector3.one, Mathf.Clamp01((TimeInLevel - 50) / 50));
+            Goal.transform.localScale = scale;
+            Goal.transform.position = new Vector3(0, 0, (1 - (TimeInLevel / 100)) * 100);
+            Goal.transform.position += new Vector3(0, scale.y / 2, 0);
         }
     }
 }
