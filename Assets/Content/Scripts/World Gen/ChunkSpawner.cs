f2f00a69843fdf98ae4045c050b670bce14ed650
ConorsFlying <53551115+ConorsFlying@users.noreply.github.com>
2024-01-27 13:30:17 +1100

    World Gen Not Done

---
+++ Assets/Content/Scripts/World Gen/ChunkSpawner.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+
+namespace AmazingAssets.CurvedWorld.Example
+{
+    public class ChunkSpawner : MonoBehaviour
+    {
+        public enum AXIS { XPositive, XNegative, ZPositive, ZNegative }
+
+        public GameObject[] chunks;
+        public int initialSpawnCount = 5;
+        public float destoryZone = 300;
+        
+
+        [HideInInspector]
+        public Vector3 moveDirection = new Vector3(-1, 0, 0);
+        public float movingSpeed = 1;
+
+
+        public float chunkSize = 60;        
+        GameObject lastChunk;
+
+
+        void Awake()
+        {
+            initialSpawnCount = initialSpawnCount > chunks.Length ? initialSpawnCount : chunks.Length;
+
+            int chunkIndex = 0;
+            for (int i = 0; i < initialSpawnCount; i++)
+            {
+                GameObject chunk = Instantiate(chunks[chunkIndex]);
+                chunk.SetActive(true);
+
+                chunk.GetComponent<RunnerChunk>().spawner = this;
+
+                chunk.transform.localPosition = new Vector3(i * chunkSize, 0, transform.position.z);
+                moveDirection = new Vector3(-1, 0, 0);
+                
+
+                lastChunk = chunk;
+
+                if (++chunkIndex >= chunks.Length)
+                    chunkIndex = 0;
+            }           
+        }
+        
+        public void DestroyChunk(RunnerChunk thisChunk)
+        {
+            Vector3 newPos = lastChunk.transform.position;
+            
+            newPos.x += chunkSize;
+
+            lastChunk = thisChunk.gameObject;
+            lastChunk.transform.position = newPos;
+        }
+    }
+}
---
+++ Assets/Content/Scripts/World Gen/RunnerChunk.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+namespace AmazingAssets.CurvedWorld.Example
+{
+    public class RunnerChunk : MonoBehaviour
+    {
+        public ChunkSpawner spawner;
+        
+
+        void Update()
+        {
+            transform.Translate(spawner.moveDirection * spawner.movingSpeed * Time.deltaTime);
+        }
+
+        void FixedUpdate()
+        {
+            if (transform.position.x < -spawner.destoryZone)
+                spawner.DestroyChunk(this);
+        }
+    }
+}
