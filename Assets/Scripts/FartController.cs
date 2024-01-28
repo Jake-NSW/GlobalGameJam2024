395bd4267485c43112b2449912c363ab23104341
SimonNordon4 <simon.nordon@protonmail.com>
2024-01-28 11:49:33 +1000

    adding poop sounds.

--- Assets/Scripts/FartController.cs
+++ Assets/Scripts/FartController.cs
@@ -3,30 +3,36 @@ using UnityEngine;
 
 namespace Jam
 {
-    [RequireComponent(typeof(AudioSource))]
     public class FartController : MonoBehaviour
-    {
+    {        
+        [field: SerializeField]
+        public bool IsFarting { get; private set; } = false;
         [SerializeField]private ParticleSystem[] fartParticles;
         [SerializeField]private AudioClip[] fartSounds;
         
         [SerializeField]private Vector2 fartIntervals = new Vector2(0.5f, 1f);
         
-        private AudioSource _audioSource;
+        [SerializeField]
+        private AudioSource audioSource;
         private float _elapsedTime = 0f;
         private float _timeToNextFart = 0f;
-
-        private void Awake()
-        {
-            _audioSource = GetComponent<AudioSource>();
-        }
-
+        
         private void Start()
         {
             _timeToNextFart = Random.Range(fartIntervals.x, fartIntervals.y);
+            
+            // Play a sound immediately when the game starts
+            if (fartSounds.Length > 0)
+            {
+                audioSource.PlayOneShot(fartSounds[0]);
+            }
         }
 
-        [field: SerializeField]
-        public bool IsFarting { get; private set; } = false;
+        private void OnEnable()
+        {
+            Fart();
+        }
+
 
         public void Fart()
         {
@@ -38,8 +44,8 @@ namespace Jam
             if (fartSounds.Length == 0) return;
             
             var randomSoundIndex = Random.Range(0, fartSounds.Length);
-            _audioSource.clip = fartSounds[randomSoundIndex];
-            _audioSource.Play();
+            var clip = fartSounds[randomSoundIndex];
+            audioSource.PlayOneShot(clip);
         }
         
         public void StartFarting()
@@ -66,3 +72,25 @@ namespace Jam
         }
     }
 }
+
+#if UNITY_EDITOR
+namespace Jam
+{
+    using UnityEditor;
+    [CustomEditor(typeof(FartController))]
+    public class FartControllerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            var fartController = (FartController) target;
+            if (GUILayout.Button("Fart"))
+            {
+                // Play a fart sound
+                fartController.Fart();
+            }
+        }
+    }
+}
+#endif
